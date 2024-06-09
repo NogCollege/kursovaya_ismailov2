5b7231cac0d24c868916b4a35337919b45f8b475
Tajik928 <isik_rish@mail.ru>
2024-06-09 21:08:36 +0300

    начало работы
    начало работы

---
+++ views/site/index.php
@@ -0,0 +1,42 @@
+<?php
+
+/** @var yii\web\View $this */
+
+$this->title = 'My Yii Application';
+?>
+<body>
+    <header class="main-header">
+        <nav>
+            <div class="container menu">
+                <ul class="site-navigation">
+                    <li><a href="">информация</a></li>
+                    <li><a href="">новости</a></li>
+                    <li><a href="">прайс-лист</a></li>
+                    <li><a href="">магазин</a></li>
+                    <li><a href="">контакты</a></li>
+                </ul>
+                <a class="vhod" href="">Вход</a>
+            </div>
+        </nav>
+    </header>
+    <footer class="podval">
+        <main class="container">
+            <p>
+                Барбершоп "Бородинский"<br>
+                Адрес: г.Санкт-Петербург, Б.Конюшенная, Д.19/8<br>
+                <a href="">Как нас найти</a><br>
+                Телефон: 8 (495) 666-02-66
+            </p>
+            <p>
+                Давайте дружить!<br>
+                <button class="cety"><img src="img/social club/vk.svg" height="30px" alt="Вк"></button>
+                <button class="cety"><img src="img/social club/facebook.svg" height="30px" alt="facebook"></button>
+                <button class="cety"><img src="img/social club/instagram.svg" height="30px" alt="instagram"></button>
+            </p>
+            <p>
+                РАЗРАБОТАНО:<br>
+                <button class="razrab">ooo</button>
+            </p>
+        </main>
+    </footer>
+</body>
